fabcedfe08bb2bda3c7f7850e7c18a73dedcdb25
collin <sscla-ops@outlook.com>
2023-11-13 13:27:57 -0500

    working

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 
 def process_inter_dealer_brokers_section(broker, index, base_file_path):
-    # Write each broker's information to a separate file
     file_name = f"{os.path.splitext(base_file_path)[0]}_interDealerBrokers_{index}.txt"
-    with open(file_name, 'w') as file:
-        file.write(str(broker))
-
+    print(f"Attempting to save file: {file_name}")  # Debugging print
+    try:
+        with open(file_name, 'w') as file:
+            file.write(str(broker))
+        print(f"File saved: {file_name}")  # Confirmation print
+    except Exception as e:
+        print(f"Error saving file {file_name}: {e}")  # Error print
 def process_file(file_path):
     data = {
         "releaseDate": "",
@@ -16,39 +19,39 @@ def process_file(file_path):
         "totals": []
     }
 
-    def process_section(section):
-        if section.startswith('releaseDate'):
-            data['releaseDate'] = section.split(':', 1)[1].strip()
-        elif section.startswith('title'):
-            data['title'] = section.split(':', 1)[1].strip()
-        elif section.startswith('numDealers'):
-            data['numDealers'] = eval(section.split(':', 1)[1].strip())
-        elif section.startswith('interDealerBrokers'):
-            inter_dealer_brokers = eval(section.split(':', 1)[1].strip())
+    def process_line(line):
+        if line.startswith('releaseDate'):
+            data['releaseDate'] = line.split(':', 1)[1].strip()
+        elif line.startswith('title'):
+            data['title'] = line.split(':', 1)[1].strip()
+        elif line.startswith('numDealers'):
+            data['numDealers'] = eval(line.split(':', 1)[1].strip())
+        elif line.startswith('interDealerBrokers'):
+            brokers_data = line.split(':', 1)[1].strip()
+            inter_dealer_brokers = eval(brokers_data)
             for index, broker in enumerate(inter_dealer_brokers):
                 data['interDealerBrokers'].append(broker)
                 process_inter_dealer_brokers_section(broker, index, file_path)
-        elif section.startswith('others'):
-            data['others'] = eval(section.split(':', 1)[1].strip())
-        elif section.startswith('totals'):
-            data['totals'] = eval(section.split(':', 1)[1].strip())
+        elif line.startswith('others'):
+            data['others'] = eval(line.split(':', 1)[1].strip())
+        elif line.startswith('totals'):
+            data['totals'] = eval(line.split(':', 1)[1].strip())
 
     try:
         with open(file_path, 'r') as file:
-            content = file.read()
-
-        sections = content.split('\n\n')
-        for section in sections:
-            process_section(section)
+            for line in file:
+                process_line(line.strip())
 
         return data
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred while processing {file_path}: {e}")
 
 # Process the quarterly data file
 file_path_quarterly = 'market_share_quarterly_fixed.txt'
+print(f"Processing file: {file_path_quarterly}")  # Debugging print
 processed_data_quarterly = process_file(file_path_quarterly)
 
 # Process the yearly data file
 file_path_yearly = 'market_share_yearly_fixed.txt'
+print(f"Processing file: {file_path_yearly}")  # Debugging print
 processed_data_yearly = process_file(file_path_yearly)
