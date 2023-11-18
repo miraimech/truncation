f0ba2e174e20d3fb19cd9512b0062afb923a30b3
collin <sscla-ops@outlook.com>
2023-11-18 05:52:59 -0700

    loop added to get all data from the file

--- main.py
+++ main.py
@@ -11,31 +11,62 @@ def truncate_text(tokens, max_length=511):
     """
     Truncate the list of tokens to the maximum length.
     """
-    return tokens[:max_length]
+    return tokens[:max_length], tokens[max_length:]
+
+def is_special_file(filename):
+    """
+    Checks if the filename is one of the special files.
+    """
+    return filename in ['marketshare_quarterly.txt', 'marketshare_yearly.txt']
+
+def process_special_file(file_path, filename, directory):
+    """
+    Processes the special file by repeatedly truncating and saving the remaining content.
+    """
+    iteration = 1
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    tokens = tokenize(content)
+    while tokens:
+        truncated_tokens, tokens = truncate_text(tokens)
+        truncated_content = ' '.join(truncated_tokens)
+
+        # Creating a new filename for the truncated file
+        new_filename = f"{filename.rsplit('.', 1)[0]}_truncated_{iteration}.txt"
+        new_file_path = os.path.join(directory, new_filename)
+
+        with open(new_file_path, 'w') as file:
+            file.write(truncated_content)
+        print(f"Processed and created truncated file: {new_filename}")
+
+        iteration += 1
 
 def process_files(directory, file_extension='.txt'):
     """
     Processes all files in the given directory with the specified file extension.
-    Creates new files with truncated content.
+    Creates new files with truncated content or handles special files differently.
     """
     for filename in os.listdir(directory):
-        if filename.endswith(file_extension):
+        if filename.endswith(file_extension) and not filename.endswith('_truncated.txt'):
             file_path = os.path.join(directory, filename)
 
-            with open(file_path, 'r') as file:
-                content = file.read()
-            
-            tokens = tokenize(content)
-            truncated_tokens = truncate_text(tokens)
-            truncated_content = ' '.join(truncated_tokens)
+            if is_special_file(filename):
+                process_special_file(file_path, filename, directory)
+            else:
+                with open(file_path, 'r') as file:
+                    content = file.read()
+                
+                truncated_tokens, _ = truncate_text(tokenize(content))
+                truncated_content = ' '.join(truncated_tokens)
 
-            # Creating a new filename for the truncated file
-            new_filename = filename.rsplit('.', 1)[0] + '_truncated.txt'
-            new_file_path = os.path.join(directory, new_filename)
+                # Creating a new filename for the truncated file
+                new_filename = filename.rsplit('.', 1)[0] + '_truncated.txt'
+                new_file_path = os.path.join(directory, new_filename)
 
-            with open(new_file_path, 'w') as file:
-                file.write(truncated_content)
-            print(f"Processed and created truncated file: {new_filename}")
+                with open(new_file_path, 'w') as file:
+                    file.write(truncated_content)
+                print(f"Processed and created truncated file: {new_filename}")
 
 # Get the directory where the script is located
 directory = os.path.dirname(os.path.abspath(__file__))
